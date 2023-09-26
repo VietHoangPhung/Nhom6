4c2840d72c7bdacd5af1d6112f69651b295460db
nguyen-thanh-dat-ktmt02 <134047230+nguyen-thanh-dat-ktmt02@users.noreply.github.com>
2023-09-26 15:39:23 +0700

    Update thong_ke.py   Tìm điểm cao nhất và thấp nhất của từng lớp và hiển thị số  sinh viên ra màn hình

--- buoi3/thong_ke.py
+++ buoi3/thong_ke.py
@@ -1,67 +1,97 @@
-# Thư viện mở file
 import pandas as pd
-
 from numpy import array
-import numpy as np
-# Thêm thư viện vẽ đồ thị
 import matplotlib.pyplot as plt
-
-df = pd.read_csv('diemPython.csv',index_col = 0,header = 0)
-# Mảng chứa dữ liệu dạng ma trận với các dòng là các hàng của dữ liệu
+import numpy as np
+df=pd.read_csv('diemPython.csv',index_col=0,header = 0)
 in_data = array(df.iloc[:,:])
 print(in_data)
 print('Tong so sinh vien di thi :')
-
-# Hàm tính % số sinh viên thi đạt điểm F
-def percentFailure (array, tongSV):
-    sum = np.sum(array)
-    average = (sum / tongSV) * 100
-    return average
-
-# Hàm in ra lớp có nhiều sinh viên giỏi nhất
-def bestLevel (array1, array2):
-    arrayGood = np.add(array1, array2)
-    max = np.max(arrayGood)
-    i, = np.where(arrayGood == max)
-    print('Lớp có nhiều học giỏi nhất là {0} có {1} sinh viên'.format(in_data[i,0],max))
+tongsv= in_data[:,1]
+print(np.sum(tongsv))
+diemA = in_data[:,3]
+diemBc = in_data[:,4]
+print('Tong sv:',tongsv)
+maxa = diemA.max()
+i = np.where(diemA == maxa)
+print('lop co nhieu diem A la {0} co {1} sv dat diem A'.format(in_data[i,0],maxa))
 
 
-# Lấy tổng toàn bộ sv trên cột 1
-tongsv = in_data[:, 1]
-tong_svThi = np.sum(tongsv)
 
-diemA = in_data[:, 3]
-diemB_plus = in_data[:, 4]
-diemB = in_data[:, 5]
-diemC_plus = in_data[:, 6]
-diemC = in_data[:, 7]
-diemD_plus = in_data[:, 8]
-diemD = in_data[:, 9]
-diemF = in_data[:, 10]
-# print(diemF)
-# print("Tổng sinh viên đạt điểm A: ", np.sum(diemA))
-# print("Tổng sinh viên đạt điểm B+: ", np.sum(diemB_plus))
-# print("Tổng sinh viên đạt điểm B: ", np.sum(diemB))
-# print("Tổng sinh viên đạt điểm C+: ", np.sum(diemC_plus))
-# print("Tổng sinh viên đạt điểm C: ", np.sum(diemC))
-# print("Tổng sinh viên đạt điểm D+: ", np.sum(diemD_plus))
-# print("Tổng sinh viên đạt điểm D: ", np.sum(diemD))
-# print("Tổng sinh viên đạt điểm F: ", np.sum(diemF))
-# print('Tong sv:', tongsv)
 
-print(f"Sinh viên thi trượt chiếm {percentFailure(diemF,tong_svThi)} %")
-bestLevel(diemA,diemB_plus)
+def diemcaonhatcuatunglop( a):
+    caclop=in_data[a,2:11]
+    print(caclop)
+    maxb = caclop.max()
+    maxbb= maxb
+    idd= np.where(caclop == maxb)
+    if maxbb==in_data[a,2]:
+        aa="Loai A+"
+        print('lop co diem cao nhat la diem {0} va co {1} sinh vien dat duoc'.format(aa, maxb))
+    elif maxbb==in_data[a,3]:
+        b="Loai A"
+        print('lop co diem cao nhat la diem {0} va co {1} sinh vien dat duoc'.format(b, maxb))
+    elif maxbb==in_data[a,4]:
+        c="Loai B+"
+        print('lop co diem cao nhat la diem {0} va co {1} sinh vien dat duoc'.format(c, maxb))
+    elif maxbb==in_data[a,5]:
+        d="Loai B"
+        print('lop co diem cao nhat la diem {0} va co {1} sinh vien dat duoc'.format(d, maxb))
+    elif maxbb==in_data[a,6]:
+        e="Loai C+"
+        print('lop co diem cao nhat la diem {0} va co {1} sinh vien dat duoc'.format(e, maxb))
+    elif maxbb==in_data[a,7]:
+        f="Loai C"
+        print('lop co diem cao nhat la diem {0} va co {1} sinh vien dat duoc'.format(f, maxb))
+    elif maxbb==in_data[a,8]:
+        g="Loai D+"
+        print('lop co diem cao nhat la diem {0} va co {1} sinh vien dat duoc'.format(g, maxb))
+    elif maxbb==in_data[a,9]:
+        h="Loai D"
+        print('lop co diem cao nhat la diem {0} va co {1} sinh vien dat duoc'.format(h, maxb))
+    elif maxbb==in_data[a,10]:
+        j="Loai F"
+        print('lop co diem cao nhat la diem {0} va co {1} sinh vien dat duoc'.format(j, maxb))
 
-# # Lớp có nhiều điểm A nhất
-# maxa = diemA.max()
-# i, = np.where(diemA == maxa)
-# print('lop co nhieu diem A la {0} co {1} sv dat diem A'.format(in_data[i,0],maxa))
-# plt.plot(range(len(diemA)),diemA,'r-',label="Diem A")
-# plt.plot(range(len(diemB)),diemB,'g-',label="Diem B +")
-# plt.xlabel('Lơp')
-# plt.ylabel(' So sv dat diem ')
-# # Đặt chú thích cho biểu đồ
-# plt.legend(loc='upper right')
-# # Hiển thị biểu đồ
-# plt.show()
-      
+
+chonlop=int(input("nhap stt lop muon chon: "))
+diemcaonhatcuatunglop(chonlop)
+
+def diemthapnhatcuatunglop( a):
+    caclop=in_data[a,2:11]
+    print(caclop)
+    maxb = caclop.min()
+    maxbb= maxb
+    idd= np.where(caclop == maxb)
+    if maxbb==in_data[a,2]:
+        aa="Loai A+"
+        print('lop co diem thap nhat la diem {0} va co {1} sinh vien dat duoc'.format(aa, maxb))
+    elif maxbb==in_data[a,3]:
+        b="Loai A"
+        print('lop co diem thap nhat la diem {0} va co {1} sinh vien dat duoc'.format(b, maxb))
+    elif maxbb==in_data[a,4]:
+        c="Loai B+"
+        print('lop co diem thap nhat la diem {0} va co {1} sinh vien dat duoc'.format(c, maxb))
+    elif maxbb==in_data[a,5]:
+        d="Loai B"
+        print('lop co diem thap nhat la diem {0} va co {1} sinh vien dat duoc'.format(d, maxb))
+    elif maxbb==in_data[a,6]:
+        e="Loai C+"
+        print('lop co diem thap nhat la diem {0} va co {1} sinh vien dat duoc'.format(e, maxb))
+    elif maxbb==in_data[a,7]:
+        f="Loai C"
+        print('lop co diem thap nhat la diem {0} va co {1} sinh vien dat duoc'.format(f, maxb))
+    elif maxbb==in_data[a,8]:
+        g="Loai D+"
+        print('lop co diem thap nhat la diem {0} va co {1} sinh vien dat duoc'.format(g, maxb))
+    elif maxbb==in_data[a,9]:
+        h="Loai D"
+        print('lop co diem thap nhat la diem {0} va co {1} sinh vien dat duoc'.format(h, maxb))
+    elif maxbb==in_data[a,10]:
+        j="Loai F"
+        print('lop co diem thap nhat la diem {0} va co {1} sinh vien dat duoc'.format(j, maxb))
+
+
+
+chonlopthapnhat=int(input("nhap stt lop muon chon: "))
+diemthapnhatcuatunglop(chonlopthapnhat)  
+
